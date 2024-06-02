d9159f344bcbaac1b5f93f02637251f1cd8c5489
Paliya Dipak <dipakthakor1938@gmail.com>
2024-06-02 10:43:05 +0530

    Day 2 task 1 another sections and footer created

---
+++ js/file1.js
@@ -0,0 +1,24 @@
+
+
+function func1(str) {
+    let var1 = document.getElementsByClassName('btn2');
+    let btnid=document.getElementById(str);
+    // Loop through all elements with the class 'btn2' and change their background color to blue
+    // for (let i = 0; i < var1.length; i++) {
+    //     var1[i].style.backgroundColor = 'black';
+    //     var1[i].style.color = 'white';
+    // }
+    btnid.style.backgroundColor='white';
+    btnid.style.color='black';
+}
+function func2(str) {
+    let var1 = document.getElementsByClassName('btn2');
+    let btnid=document.getElementById(str);
+    // Loop through all elements with the class 'btn2' and change their background color to blue
+    // for (let i = 0; i < var1.length; i++) {
+    //     var1[i].style.backgroundColor = 'white';
+    //     var1[i].style.backgroundColor = 'black';
+    // }
+    btnid.style.backgroundColor='black';
+    btnid.style.color='white';
+}
