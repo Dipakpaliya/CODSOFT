47b315b1acc9bc9b10190dda208f8c86ea5d08cc
Paliya Dipak <dipakthakor1938@gmail.com>
2024-06-05 00:15:45 +0530

    Day 5 task 2 Added some modifications

---
+++ js/file2.js
@@ -0,0 +1,12 @@
+console.log("aLL OK");
+
+function menu(str){
+    let a=document.getElementById('str');
+    if(a.style.display=='none'){
+        a.style.display='block';
+    }
+    else{
+        a.style.display='none';
+}
+}
+
