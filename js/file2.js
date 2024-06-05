505f89332e1633a3474fe889f529dfc1869f3f1d
Paliya Dipak <dipakthakor1938@gmail.com>
2024-06-05 13:20:31 +0530

    day 5 task 2 Added responsiveness and form

--- js/file2.js
+++ js/file2.js
@@ -1,7 +1,9 @@
 console.log("aLL OK");
 
 function menu(str){
-    let a=document.getElementById('str');
+    console.log("here");
+    let a=document.getElementById(str);
+    
     if(a.style.display=='none'){
         a.style.display='block';
     }
@@ -9,4 +11,7 @@ function menu(str){
         a.style.display='none';
 }
 }
-
+let var1=document.getElementById('hammenu');
+if(window.innerWidth > 1200){
+    var1.style.display='none';
+}
